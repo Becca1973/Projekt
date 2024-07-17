3275eb02747e2e7403c62fa4f3f828d14a1b5e5d
rebekacep <rebeka.cep@student.um.si>
2024-07-17 12:52:41 +0200

    Added instagram api path

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const multer = require("multer");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const facebook = require("./api/facebook");
+const instagram = require("./api/instagram"); // Import Instagram API
 require("dotenv").config();
 
 const app = express();
@@ -20,9 +21,15 @@ const upload = multer({ storage: storage });
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use("/uploads", express.static("uploads"));
 
 // API poti
 app.use("/api/facebook", upload.single("image"), facebook);
+app.use("/api/instagram", upload.single("image"), instagram); // Add Instagram route
+
+app.get("/", (req, res) => {
+  res.send("Strežnik je aktiven.");
+});
 
 app.listen(5000, () => {
   console.log("Server is running on port 5000");
