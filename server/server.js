f25d86c08e5236fa72a8d45bbc9dac4d6c2f87e4
--global <olga.jokic@student.um.si>
2024-07-24 18:52:27 +0200

    Added api for Reddit

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ const facebook = require("./api/facebook");
 const instagram = require("./api/instagram");
 const linkedin = require("./api/linkedIn");
 const twitter = require("./api/twitter");
+const reddit = require("./api/reddit");
 
 require("dotenv").config();
 
@@ -32,6 +33,7 @@ app.use("/api/facebook", upload.single("image"), facebook);
 app.use("/api/instagram", upload.single("image"), instagram);
 app.use("/api/linkedin", upload.single("image"), linkedin);
 app.use("/api/twitter", upload.single("image"), twitter);
+app.use("/api/reddit", upload.single("image"), reddit);
 
 app.get("/", (req, res) => {
   res.send("Strežnik je aktiven.");
